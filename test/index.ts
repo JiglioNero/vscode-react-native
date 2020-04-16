4772e71e3775d36276ea3de5afcf632f258554d6
Mikhail Suendukov <mikhail.suendukov@inyar.ru>
2020-04-16 14:06:53 +0300

    fix testRoot

--- test/index.ts
+++ test/index.ts
@@ -23,12 +23,12 @@ export function run(): Promise<void> {
     mocha.useColors(true);
     mocha.invert();
 
-    const testsRoot = path.resolve(__dirname, "..");
+    const testsRoot = path.resolve(__dirname);
     // Register Mocha options
-    return new Promise((c, e) => {
+    return new Promise((resolve, reject) => {
         glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
           if (err) {
-            return e(err);
+            return reject(err);
           }
 
           // Add files to the test suite
@@ -38,13 +38,13 @@ export function run(): Promise<void> {
             // Run the mocha test
             mocha.run((failures: any) => {
               if (failures > 0) {
-                e(new Error("${failures} tests failed."));
+                reject(new Error("${failures} tests failed."));
               } else {
-                c();
+                resolve();
               }
             });
           } catch (err) {
-            e(err);
+            reject(err);
           }
         });
       });
